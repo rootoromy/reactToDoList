d8baecc996e890c3de6c5d499ce28f89ff05e1d8
bu-yasutake <h.yasutake@brewus.co.jp>
2024-10-22 12:30:04 +0900

    styled-componentsのエラーが解消されない

--- ReactToDoList/src/App.jsx
+++ ReactToDoList/src/App.jsx
@@ -2,7 +2,22 @@
 //import styled from "styled-components";
 //import styled, { css } from 'styled-components'
 //import './App.scss'
-import { Container,Button } from "./styles";
+//import { Container,Button } from "./styles";
+import styled from "styled-components";
+
+const Button = styled.button`
+font-size: 1.5em;
+text-align: center;
+color: red;
+border: 2px solid #BF4F74;
+`;
+
+const Container = styled.section`
+font-size: 1.5em;
+text-align: center;
+color: blue;
+background-color: yellow;
+`;
 
 function App() {
   return (
