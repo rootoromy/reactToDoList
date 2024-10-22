a3b6b5fc42a54292d596cbaefe211dce2a5f9b57
bu-yasutake <h.yasutake@brewus.co.jp>
2024-10-22 18:58:56 +0900

    input欄とボタン欄を実装

--- ReactToDoList/src/App.jsx
+++ ReactToDoList/src/App.jsx
@@ -5,24 +5,37 @@
 //import { Container,Button } from "./styles";
 import styled from "styled-components";
 
+/*
 const GlobalStyle = createGlobalStyle`
 body {
   justify-content:center;
 }
 `;
+*/
 
 const Button = styled.button`
-font-size: 1.5em;
+font-size: 1rem;
 text-align: center;
 color: red;
 border: 2px solid #BF4F74;
 `;
 
 const Container = styled.section`
-font-size: 1.5em;
+font-size: 1rem;
 text-align: center;
 color: blue;
 background-color: yellow;
+padding: 30px;
+`;
+
+const Input = styled.input`
+width: 300px;
+height: 30px;
+`;
+
+const InputWrap = styled.div`
+display: flex;
+flex-direction: row;
 `;
 
 function App() {
@@ -30,8 +43,10 @@ function App() {
     <>
       <Container>
         <h1>TO DO List</h1>
-        <Button>Normal Button</Button>
-        <Button $primary>Primary Button</Button>
+        <InputWrap className="inputWrap">
+          <Input type="text" />
+          <Button>TODO</Button>
+        </InputWrap>
       </Container>
     </>
   )
