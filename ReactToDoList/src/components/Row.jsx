8f7f82fcacabc06c1564f70420dc93fa63a1ea34
bu-yasutake <h.yasutake@brewus.co.jp>
2024-11-16 16:58:54 +0900

    FB対応

--- ReactToDoList/src/components/Row.jsx
+++ ReactToDoList/src/components/Row.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import { TodoTr, DeleteButton, InputText } from "./styles";
 import ToggleButton from "./Toggle";
+import { v4 as uuidv4 } from "uuid";
 
 
 
-const Row = ({ todo, onDelete, onComplete }) => {
+const Row = ({ todo, onDelete, onComplete, onEdit }) => {
   const [todoEdit, setTodoEdit] = useState(todo.text);
 
   const EditTextArea = (event) => {
-    setTodoEdit(event.target.value);
+    const updatedText = event.target.value;
+    setTodoEdit(updatedText);
+    onEdit(todo.id, updatedText);
   };
 
   //「どのTodoが完了したのか？」ということをデータとして保持したい。
@@ -19,11 +22,12 @@ const Row = ({ todo, onDelete, onComplete }) => {
           type="text"
           value={todoEdit}
           onChange={EditTextArea}
+          id={uuidv4()}
         />
 
       </td>
       <td>
-        <ToggleButton todo={todo} onComplete={onComplete} />
+        <ToggleButton todo={todo} onComplete={onComplete} onEdit={onEdit}/>
       </td>
       <td>
         <DeleteButton onClick={() => onDelete(todo.id)}>Delete</DeleteButton>
